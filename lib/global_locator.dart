cbc29b87024051ba398687b0efa4d3b0041841bc
Alejo Muñoz <alejandro.munoz@imagineapps.co>
2023-08-19 16:50:35 -0500

    register repository to characters

--- lib/global_locator.dart
+++ lib/global_locator.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
 import 'src/api/api_repository.dart';
+import 'src/bloc/rick_and_morty/repository/character_repository.dart';
 import 'src/bloc/rick_and_morty/rick_and_morty_bloc.dart';
 
 late GetIt global;
@@ -11,4 +12,6 @@ void setupGlobalLocator() {
   global.registerLazySingleton(() => Logger());
   global.registerFactory<APIRepository>(() => DefaultAPIRepository());
   global.registerLazySingleton<RickAndMortyBloc>(() => RickAndMortyBloc());
+  global
+      .registerFactory<CharacterRepository>(() => CharacterRepositoryDefault());
 }
