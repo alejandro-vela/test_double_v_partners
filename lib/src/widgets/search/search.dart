9460e525394d1e862a77c0cc190121621b3a1896
Alejo Muñoz <alejandro.munoz@imagineapps.co>
2023-08-19 19:11:42 -0500

    add search bar

---
+++ lib/src/widgets/search/search.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import '../../bloc/rick_and_morty/model/model_characters.dart';
+
+class CustomSearchDelegate extends SearchDelegate {
+  CustomSearchDelegate({required this.dataList});
+  final List<NameIndex> dataList;
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const SizedBox();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<NameIndex> suggestions = dataList;
+
+    if (query.isNotEmpty) {
+      suggestions = dataList
+          .toList()
+          .where((element) =>
+              element.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    }
+
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, i) {
+        return ListTile(
+          title: Text(suggestions[i].name,
+              style: Theme.of(context).textTheme.bodyMedium),
+          onTap: () {
+            //to-do: Navigate to character details
+            close(context, null);
+          },
+        );
+      },
+    );
+  }
+}
